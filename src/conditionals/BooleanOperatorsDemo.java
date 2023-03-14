7ccc894fd3511c3fcaf8e8f5df111be646086d18
Chuck Kiefriter <124002280+ckiefriter1@users.noreply.github.com>
2023-03-14 16:43:21 -0400

    Minor changes

--- src/conditionals/BooleanOperatorsDemo.java
+++ src/conditionals/BooleanOperatorsDemo.java
@@ -21,11 +21,18 @@ public class BooleanOperatorsDemo {
 			System.out.println("Condition evaluated false");
 		}
 		
-		// Boolean expression logical AND (&&), OR (||) expressions 
+		System.out.println("-------------------------------------------------------------");
+		
+		// Boolean expression logical AND (&&) operator in expressions 
 		System.out.println(trueExpression + " expression AND (&&) " + trueExpression + " expression evaluates " + (trueExpression && trueExpression));
 		System.out.println(trueExpression + " expression AND (&&) " + falseExpression + " expression evaluates " + (trueExpression && falseExpression));
 		
-		System.out.println(trueExpression + " expression OR (||) " + trueExpression + " expression evaluates " + (trueExpression && trueExpression));
+		System.out.println("-------------------------------------------------------------");
+		
+		// Boolean expression logical OR (||) operator in expressions 
+		System.out.println(trueExpression + " expression OR (||) " + trueExpression + " expression evaluates " + (trueExpression || trueExpression));
+		System.out.println(trueExpression + " expression OR (||) " + falseExpression + " expression evaluates " + (trueExpression || falseExpression));
+		System.out.println(falseExpression + " expression OR (||) " + falseExpression + " expression evaluates " + (falseExpression || falseExpression));
 		
 	}
 
