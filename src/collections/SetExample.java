9e81de763b4c53ebd3dc9aab491a6dd3b17d7f9f
Chuck Kiefriter <124002280+ckiefriter1@users.noreply.github.com>
2023-03-27 12:28:01 -0400

    strings code examples

---
+++ src/collections/SetExample.java
@@ -0,0 +1,36 @@
+package collections;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class SetExample {
+
+	public static void main(String[] args) {
+		Set<String> vegetables = new HashSet<String>();
+
+		vegetables.add("Carrot");
+		System.out.println(vegetables);
+		
+		vegetables.add("Onion");
+		System.out.println(vegetables);
+		
+		vegetables.add("Broccoli");
+		System.out.println(vegetables);
+		
+		vegetables.add("Asparagus");
+		System.out.println(vegetables);
+		
+		vegetables.add("Carrot");
+		System.out.println(vegetables);
+
+		vegetables.add(null);
+		System.out.println(vegetables);
+
+		vegetables.add(null);
+		System.out.println(vegetables);
+
+		System.out.println("The size of our set is: " + vegetables.size());
+
+	}
+
+}
