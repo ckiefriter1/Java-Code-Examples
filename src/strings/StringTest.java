06c85ce1b0a4f88c557b88daffb29517e2d80831
Chuck Kiefriter <124002280+ckiefriter1@users.noreply.github.com>
2023-03-27 09:22:57 -0400

    strings code examples

---
+++ src/strings/StringTest.java
@@ -0,0 +1,75 @@
+package strings;
+
+import java.util.GregorianCalendar;
+
+public class StringTest {
+
+	/*
+	 * Compare run-times and memory usage for StringBuffer vs StringBuilder classes.
+	 */
+	public static void main(String[] args) {
+		testString();
+		System.out.println("------------------------------------------");
+		//testStringBuffer();
+		System.out.println("------------------------------------------");
+		//testStringBuilder();
+	}
+	
+	public static void testString() {
+		System.gc();
+		long start=new GregorianCalendar().getTimeInMillis();
+		long startMemory=Runtime.getRuntime().freeMemory();
+		
+		String s = ":";
+		
+		for(int i = 0; i<50000; i++){
+			s += ":" + Integer.toString(i);
+		}
+		
+		long end=new GregorianCalendar().getTimeInMillis();
+		long endMemory=Runtime.getRuntime().freeMemory();
+		
+		System.out.println("*** String Stats***");
+		System.out.println("Time Taken: "+(end-start) + " milliseconds");
+		System.out.println("Memory used: " + String.format("%,d",(startMemory-endMemory)));
+	}
+
+	public static void testStringBuffer() {
+		System.gc();
+		long start=new GregorianCalendar().getTimeInMillis();
+		long startMemory=Runtime.getRuntime().freeMemory();
+		
+		StringBuffer sb = new StringBuffer();
+		
+		for(int i = 0; i<10000000; i++){
+			sb.append(":").append(i);
+		}
+		
+		long end=new GregorianCalendar().getTimeInMillis();
+		long endMemory=Runtime.getRuntime().freeMemory();
+		
+		System.out.println("*** StringBuffer Stats***");
+		System.out.println("Time Taken: "+(end-start) + " milliseconds");
+		System.out.println("Memory used: " + String.format("%,d",(startMemory-endMemory)));
+	}
+
+	public static void testStringBuilder() {
+		System.gc();
+		long start=new GregorianCalendar().getTimeInMillis();
+		long startMemory=Runtime.getRuntime().freeMemory();
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(int i = 0; i<10000000; i++){
+			sb.append(":").append(i);
+		}
+		
+		long end=new GregorianCalendar().getTimeInMillis();
+		long endMemory=Runtime.getRuntime().freeMemory();
+		
+		System.out.println("*** StringBuilder Stats***");
+		System.out.println("Time Taken: "+(end-start) + " milliseconds");
+		System.out.println("Memory used: " + String.format("%,d",(startMemory-endMemory)));
+	
+	}
+}
