e05e5906fd3c728cb2b286ca0cef10911a8688fb
Chuck Kiefriter <124002280+ckiefriter1@users.noreply.github.com>
2023-04-02 18:55:47 -0400

    week 5

---
+++ src/classes/Animal.java
@@ -0,0 +1,84 @@
+package classes;
+
+public class Animal {
+	
+	/*
+	 * Class properties (variables), the data that's stored in objects (instances) of 
+	 * this class. 
+	 */
+	String name;
+	String type;
+	double weight;
+	double height;
+	String location;
+	
+	/*
+	 * No argument Constructor.
+	 */
+	public Animal() { }
+	
+	/*
+	 * Constructor that utilized to pass all the object properties to it as part of 
+	 * instantiating the object.
+	 */
+	public Animal(String name, String type, double weight, double height, String location ) {
+		this.name = name;
+		this.type = type;
+		this.weight = weight;
+		this.height = height;
+		this.location = location;
+	}
+	
+	/*
+	 * Getters / Setters for access to all properties.
+	 */
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public void describe() {
+		System.out.println("Animal Description");
+		System.out.println("------------------");
+		System.out.println("\tName: " + name);
+		System.out.println("\tType: " + type);
+		System.out.println("\tWeight: " + weight + " lb.");
+		System.out.println("\tHeight: " + height + " in.");
+		System.out.println("\tLocated at: " + location);
+	}
+}
---
+++ src/classes/GetterSetterExample.java
@@ -0,0 +1,29 @@
+package classes;
+
+public class GetterSetterExample {
+	
+	String name;
+	
+	/*
+	 * Getter method to get (retrieve) name property.
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/*
+	 * Setter method to set (assign a value to) name property.
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public static void main(String[] args) {
+		
+		GetterSetterExample gs = new GetterSetterExample();
+		gs.setName("Chuck Kiefriter");
+		System.out.println("My name is: " + gs.getName());
+
+	}
+
+}
