f6e0ddb83da650b7cb954dcac082570cdd79ee7e
Chuck Kiefriter <124002280+ckiefriter1@users.noreply.github.com>
2023-04-03 13:11:59 -0400

    week 5

---
+++ src/exceptions/ExceptionExample.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class ExceptionExample {
+
+	public static void main(String[] args) {
+		
+		ListOfNumbers list = new ListOfNumbers();
+		
+		list.writeList();
+	}
+
+}
---
+++ src/exceptions/ListOfNumbers.java
@@ -0,0 +1,48 @@
+package exceptions;
+
+import java.util.*;
+import java.io.*;
+
+public class ListOfNumbers {
+	private List<Integer> list = new ArrayList<Integer>();
+	private static final int SIZE = 10;
+
+	public ListOfNumbers() {
+		/*
+		for (int i = 1; i <= SIZE; i++) {
+			Integer num = (i * SIZE);
+			list.add(num);
+		}
+		*/
+	}
+	
+	public void writeList() {
+	    PrintWriter out = null;
+
+	    try {
+	        System.out.println("Entering \"try\" statement");
+
+	        out = new PrintWriter(new FileWriter("OutFile.txt"));
+	        
+	        for (int i = 0; i < SIZE; i++) {
+	        	out.println("Value at index: " + i + " = " + list.get(i));
+	            System.out.println("Value at index: " + i + " = " + list.get(i));
+	        }
+	    } catch (IndexOutOfBoundsException e) {
+	        System.err.println("Caught IndexOutOfBoundsException: " +  e.getMessage());
+	                                 
+	    } catch (IOException e) {
+	        System.err.println("Caught IOException: " +  e.getMessage());
+	                                 
+	    } finally {
+	        if (out != null) {
+	            System.out.println("*** Closing PrintWriter");
+	            out.close();
+	        } 
+	        else {
+	            System.out.println("*** PrintWriter not open");
+	        }
+	    }
+	}
+
+}
