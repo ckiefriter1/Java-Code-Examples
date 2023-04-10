f3a8f1c038f0eebb8e333ca2f95e20bdc3359784
Chuck Kiefriter <124002280+ckiefriter1@users.noreply.github.com>
2023-04-09 22:09:57 -0400

    week 6

---
+++ src/UnitTesting/Math.java
@@ -0,0 +1,30 @@
+package UnitTesting;
+
+public class Math {
+
+	public int multiply(int a, int b) {
+		return a * b;
+	}
+	
+	public double divide(double a, double b) {
+		return a / b;
+	}
+	
+	public int divide(int a, int b) {
+		return a / b;
+	}
+	
+	public boolean isPositiveNumber(int a) {
+		return a > 0;
+	}
+
+	
+	public static void main(String[] args) {
+		Math m = new Math();
+		int a = 5;
+		int b = 6;
+		System.out.println(a + " * " + b + " = " + m.multiply(a, b));
+
+	}
+
+}
