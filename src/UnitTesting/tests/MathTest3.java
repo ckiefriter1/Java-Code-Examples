bb0f370ae429324a2107a5ea7e9d18c7f78097f9
Chuck Kiefriter <ckiefriter@gmail.com>
2025-01-21 21:17:42 -0500

    week 6

--- src/UnitTesting/tests/MathTest3.java
+++ src/UnitTesting/tests/MathTest3.java
@@ -2,18 +2,52 @@ package UnitTesting.tests;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import UnitTesting.Math;
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import UnitTesting.Math;
 
 class MathTest3 {
 
-	Math m = new Math();
+	Math m;
 	
+	@BeforeEach
+	void setUp() throws Exception {
+		m = new Math();
+	}
+
 	@Test
 	void testMultiplyReturnsCorrectValues() {
 		int product = m.multiply(2, 5);
 		assertEquals(10, product);
 	}
 	
+	@Test
+	void testDivideReturnsCorrectValues() {
+		double product = m.divide(5.0, 2.0);
+		assertEquals(2.5, product);
+	}
+	
+	@Test
+	void testDivideIntThrowsExceptionWhenDivideByZero() {
+		assertThrows(ArithmeticException.class, () -> m.divide(5, 0));
+	}
+	
+	@Test
+	void testDivideDoubleThrowsExceptionWhenDivideByZero() {
+		assertEquals(Double.POSITIVE_INFINITY, m.divide(5.0, 0.0));
+	}
+	
+	@Test
+	void testIsPositiveNumberReturnsTrueIfArgIsPositive() {
+		int a = 5;
+		assertTrue(m.isPositiveNumber(a));
+	}
+	
+	@Test
+	void testIsPositiveNumberReturnsFalseIfArgIsNegative () {
+		int a = -5;
+		assertFalse(m.isPositiveNumber(a));
+	}
+
 }
